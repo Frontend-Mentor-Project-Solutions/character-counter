05b370e719cc1da1973fa446e5f42b99de7a80ff
Kalil Subaan Buraaleh <91347702+kalilsub@users.noreply.github.com>
2026-05-26 13:09:05 +0200

    handle character limit setting and validation

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ const textArea = document.querySelector("textarea");
 const excludeSpacesCheckbox = document.querySelector(
   "input[name=exclude-spaces]",
 );
+const characterLimitCheckbox = document.querySelector(
+  "input[name=character-limit-checkbox]",
+);
+const characterLimitInput = document.querySelector(
+  "input[name=character-limit-input]",
+);
 
 const graphemeSegmenter = new Intl.Segmenter(undefined, {
   granularity: "grapheme",
@@ -17,9 +23,20 @@ const countFormat = new Intl.NumberFormat(undefined, {
   minimumIntegerDigits: 2,
 });
 
-textArea.addEventListener("input", updateCounters);
+textArea.addEventListener("input", () => {
+  updateCounters();
+  validateTextArea();
+});
 excludeSpacesCheckbox.addEventListener("change", updateCounters);
+characterLimitCheckbox.addEventListener("change", () => {
+  toggleNumberInput();
+  validateTextArea();
+});
+characterLimitInput.addEventListener("input", () => {
+  validateTextArea();
+});
 
+// TODO: should also run after page load in case the textarea is pre-filled
 function updateCounters() {
   const text = textArea.value;
 
@@ -43,3 +60,56 @@ function updateCounters() {
     ).length,
   );
 }
+
+function validateTextArea() {
+  textArea.setCustomValidity("");
+
+  const characterLimit = getCharacterLimit();
+
+  if (!characterLimit) {
+    textArea.setCustomValidity("");
+
+    displayErrorMessage();
+    return;
+  }
+
+  if (textArea.value.length > characterLimit) {
+    textArea.setCustomValidity(
+      `Limit reached! Your text exceeds ${characterLimit} characters.`,
+    );
+    displayErrorMessage();
+  }
+}
+
+// TODO: should also run after page load in case the checkbox is pre-checked
+function toggleNumberInput() {
+  if (characterLimitCheckbox.checked) {
+    characterLimitInput.hidden = false;
+    characterLimitInput.value = "";
+  } else {
+    characterLimitInput.hidden = true;
+  }
+}
+
+function getCharacterLimit() {
+  if (!characterLimitCheckbox.checked) {
+    return;
+  }
+
+  if (characterLimitInput.validity.valid) {
+    return characterLimitInput.value;
+  }
+}
+
+function displayErrorMessage() {
+  const errorMessage = document.querySelector(".error-message");
+  const infoIcon = "./assets/images/info-icon.svg";
+
+  errorMessage.textContent = textArea.validationMessage;
+}
+
+// On page load
+// firefox only, use domcontentloaded
+// or maybe reset all fields on pageload
+toggleNumberInput();
+updateCounters();
